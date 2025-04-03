dec6e87f7d66c09f9456354f7fa21cc345ab1472
Scavable <9273772+Scavable@users.noreply.github.com>
2025-04-03 10:24:06 -0500

    Optimizations
    Optimized bot.ts (entry point).
    
    Moved the commands update functions to a separate class for better modularity and readability.

--- src/bot.ts
+++ src/bot.ts
@@ -1,78 +1,31 @@
 import { config } from "./config";
-import { Client, Collection, GatewayIntentBits, REST, Routes } from "discord.js";
-
+import { Client, Collection, GatewayIntentBits } from "discord.js";
 import EventLoader from "./utility/eventLoader";
 import CommandLoader from "./utility/commandLoader";
+import CommandSync from "./utility/commandSync";
 import AMP from "./amp/amp";
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
-const rest = new REST().setToken(config.DISCORD_TOKEN);
-
-let commandsCollection: Collection<String, any> = new Collection();
 client.commands = new Collection();
 
-// Main Execution (Ensures all async steps are completed sequentially)
 (async () => {
     try {
         console.log("Starting bot...");
 
-        // Load events using EventLoader
-        const eventLoader = new EventLoader(client);
-        eventLoader.loadEvents();
-
-        // Load commands using CommandLoader
-        const commandLoader = new CommandLoader(client, commandsCollection);
+        new EventLoader(client).loadEvents();
+        const commandLoader = new CommandLoader(client, client.commands);
         await commandLoader.loadCommands();
 
-        // Sync guild commands
-        await syncGuildCommands();
+        const commandSync = new CommandSync(client);
+        await commandSync.syncGuildCommands();
 
-        // Log in to Discord
         await client.login(config.DISCORD_TOKEN);
         console.log("Bot successfully logged in!");
 
         const amp = AMP.getInstance();
         amp.setCredentials(config.AMP_USERNAME, config.AMP_PASS, "", false);
         await amp.login();
-        //console.log(await amp.getInstances());
-
     } catch (error) {
         console.error("Error during bot initialization:", error);
     }
 })();
-
-async function syncGuildCommands(forceUpdate?: boolean) {
-    try {
-        const guildCommands = await rest.get(Routes.applicationGuildCommands(config.CLIENT_ID, config.GUILD_ID));
-
-        if (isCommandUpdateNeeded(guildCommands, commandsCollection) || forceUpdate) {
-            console.log("Updating guild commands...");
-            commandsCollection = new Collection();
-
-            const commandLoader = new CommandLoader(client, commandsCollection);
-            await commandLoader.loadCommands();
-
-            let commands = Array.from(commandsCollection.values());
-
-            await rest.put(
-                Routes.applicationGuildCommands(config.CLIENT_ID, config.GUILD_ID),
-                { body: commands }
-            );
-
-            console.log(`Successfully registered ${commandsCollection.size} commands.`);
-        } else {
-            console.log("No command updates required.");
-        }
-    } catch (error) {
-        console.error('Error syncing guild commands:', error);
-    }
-}
-
-function isCommandUpdateNeeded(guildCommands, localCommands: Collection<String, any>) {
-    if (guildCommands.length !== localCommands.size) return true;
-
-    return guildCommands.some(guildCommand => {
-        const localCommand = localCommands.get(guildCommand.name);
-        return !localCommand || guildCommand.name !== localCommand.name;
-    });
-}
---
+++ src/utility/commandSync.ts
@@ -0,0 +1,40 @@
+import { REST, Routes } from "discord.js";
+import { config } from "../config";
+import CommandLoader from "./commandLoader";
+
+export default class CommandSync {
+    constructor(client) {
+        this.client = client;
+        this.rest = new REST().setToken(config.DISCORD_TOKEN);
+    }
+
+    async syncGuildCommands(forceUpdate = false) {
+        try {
+            const guildCommands = await this.rest.get(Routes.applicationGuildCommands(config.CLIENT_ID, config.GUILD_ID));
+
+            if (this.isCommandUpdateNeeded(guildCommands) || forceUpdate) {
+                console.log("Updating guild commands...");
+                this.client.commands.clear();
+                await new CommandLoader(this.client, this.client.commands).loadCommands();
+
+                await this.rest.put(Routes.applicationGuildCommands(config.CLIENT_ID, config.GUILD_ID), {
+                    body: Array.from(this.client.commands.values()),
+                });
+
+                console.log(`Successfully registered ${this.client.commands.size} commands.`);
+            } else {
+                console.log("No command updates required.");
+            }
+        } catch (error) {
+            console.error("Error syncing guild commands:", error);
+        }
+    }
+
+    isCommandUpdateNeeded(guildCommands) {
+        if (guildCommands.length !== this.client.commands.size) return true;
+        return guildCommands.some(guildCommand => {
+            const localCommand = this.client.commands.get(guildCommand.name);
+            return !localCommand || guildCommand.name !== localCommand.name;
+        });
+    }
+}
