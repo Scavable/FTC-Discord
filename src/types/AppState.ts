971be8cbc804b7b3d42507548077ac5b35bd6590
Scavable <9273772+Scavable@users.noreply.github.com>
2026-02-18 19:57:26 -0600

    Sync: Restore missing files and update command references

--- src/types/AppState.ts
+++ src/types/AppState.ts
@@ -1,2 +1,2 @@
 export enum AppState { Offline = 0, Starting = 10, Online = 20, Suspended = -1}
-export const MetricKey = {ActiveUsers: 'Active Users'} as const;
+export const MetricKey = {ActiveUsers: "Active Users"} as const;
--- src/utility/Commands.ts
+++ src/utility/Commands.ts
@@ -7,7 +7,7 @@ import CommandButtons from '../commands/discord/command_buttons';
 import { REST, Routes } from 'discord.js';
 import { config } from '../Config';
 import logger from './Logger';
-import ServerInformationCommand from '../commands/amp/server_information';
+import ServerInformationCommand from '../commands/amp/servers_panel';
 
 class Commands {
   private client: CustomClient;
